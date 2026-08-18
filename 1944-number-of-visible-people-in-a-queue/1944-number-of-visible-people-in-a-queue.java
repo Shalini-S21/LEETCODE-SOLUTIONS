856675265b368a83e48d47300a9aa9cbb9fa6ace
Shalini S <shalini.sk186@gmail.com>
2026-08-18 14:29:27 +0530

    Time: 20 ms (95.61%), Space: 116.3 MB (67.42%) - LeetHub

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] canSeePersonsCount(int[] heights) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        int n = heights.length;
+        int[] arr = new int[n];
+
+        for(int i=n-1;i>=0;i--) {
+            int count = 0;
+
+            while(!dq.isEmpty() && dq.peek() < heights[i]) {
+                dq.pop();
+                count++;
+            }
+
+            if(!dq.isEmpty())
+                count++;
+
+            arr[i] = count;
+
+            dq.push(heights[i]);
+        }
+
+        return arr;
+    }
+}
