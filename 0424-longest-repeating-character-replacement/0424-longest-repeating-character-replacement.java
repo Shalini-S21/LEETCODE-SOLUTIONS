deaf05698e29e3d83e20079231678f77c1922d75
Shalini S <shalini.sk186@gmail.com>
2026-08-20 15:01:17 +0530

    Time: 7 ms (88.99%), Space: 46.3 MB (38.44%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,42 +1,18 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-       int[] arr = new int[26];
-       int i=0;
-       int count=0;
-       int maxlen=0;
-       for(int j=0;j<s.length();j++){
-            arr[s.charAt(j)-'A']+=1;
-            count = Math.max(count,arr[s.charAt(j)-'A']);
-            while((j-i+1)-count>k){
-                arr[s.charAt(i)-'A']-=1;
-                i++;
+        int[] arr = new int[26];
+        int count=0;
+        int l=0;
+        int max=0;
+        for(int r=0;r<s.length();r++){
+            arr[ (s.charAt(r)-'A')] +=1;
+            count = Math.max(count,arr[s.charAt(r)-'A']);
+            while((r-l+1) - count>k){
+                arr[(s.charAt(l)-'A')]-=1;
+                l++;
             }
-            maxlen=Math.max(maxlen,j-i+1);
-       }
-      return maxlen;
+            max=Math.max(max,r-l+1);
+        }
+        return max;
     }
 }
-// class Solution {
-//     public int characterReplacement(String s, int k) {
-//        char[] ch = s.toCharArray();
-//        int i=0;
-//        int j=1;
-//        int temp=k;
-//        int ans = 0;
-//        while(j<s.length() && i<s.length()){
-//         if(ch[i]==ch[j])
-//         j++;
-//         else if(ch[i]!=ch[j] && k!=0)
-//             {
-//                 k--;
-//                 j++;
-//             }
-//             else {
-//                 k=temp;
-//                 i=j;
-//             }
-//         ans = Math.max(ans,j-i);
-//        }
-//        return ans;
-//     }
-// }
