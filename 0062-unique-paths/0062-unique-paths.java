a2b454e1bdafbada65d0a2ce38599ff883f9c592
Shalini S <shalini.sk186@gmail.com>
2026-09-01 15:09:44 +0530

    Time: 54 ms (0.78%), Space: 41.2 MB (99.93%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solve(int i, int j, int m, int n, int[][] dp) {
+        if(i == m - 1 && j == n - 1)
+            return 1;
+        if(i >= m || j >= n)
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        int down = solve(i + 1, j, m, n, dp);
+        int right = solve(i, j + 1, m, n, dp);
+        dp[i][j] = down + right;
+        return dp[i][j];
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++)
+            Arrays.fill(dp[i], -1);
+        return solve(0, 0, m, n, dp);
+    }
+}
