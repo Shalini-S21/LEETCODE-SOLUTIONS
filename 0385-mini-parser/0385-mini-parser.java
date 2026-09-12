994adc9d6365834a7f2788c7fd8eba9bff0892a7
Shalini S <shalini.sk186@gmail.com>
2026-09-12 14:40:20 +0530

    Time: 7 ms (60%), Space: 47.8 MB (31.53%) - LeetHub

---
+++ 0385-mini-parser/0385-mini-parser.java
@@ -0,0 +1,91 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+   class Solution {
+    public NestedInteger deserialize(String s) {
+
+        if (!s.startsWith("["))
+            return new NestedInteger(Integer.parseInt(s));
+
+        Stack<NestedInteger> st = new Stack<>();
+
+        int num = 0;
+        boolean negative = false;
+        boolean hasNum = false;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '[') {
+
+                st.push(new NestedInteger());
+
+            } 
+            else if (ch == '-') {
+
+                negative = true;
+
+            } 
+            else if (Character.isDigit(ch)) {
+
+                num = num * 10 + (ch - '0');
+                hasNum = true;
+
+            } 
+            else if (ch == ',' || ch == ']') {
+
+                // If a number was built, add it
+                if (hasNum) {
+
+                    if (negative)
+                        num = -num;
+
+                    st.peek().add(new NestedInteger(num));
+
+                    num = 0;
+                    negative = false;
+                    hasNum = false;
+                }
+
+                // Closing current list
+                if (ch == ']') {
+
+                    NestedInteger curr = st.pop();
+
+                    if (!st.isEmpty())
+                        st.peek().add(curr);
+                    else
+                        return curr;
+                }
+            }
+        }
+
+        return null;
+    }
+}
